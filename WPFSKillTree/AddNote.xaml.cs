cb1be6ee5e3f4b22e02ef26e864d1c12a451e239
Emmitt Johnson <1337ERJ1337@gmail.com>
2014-08-24 17:30:14 -0700

    AddNote Files

---
+++ WPFSKillTree/AddNote.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using MahApps.Metro.Controls;
+
+namespace POESKillTree.Views
+{
+    /// <summary>
+    /// Interaction logic for AddNote.xaml
+    /// </summary>
+    public partial class AddNote : MetroWindow
+    {
+        public AddNote()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        public string getNote()
+        {
+            return txtName.Text;
+        }
+
+        private void AddNote_Loaded(object sender, RoutedEventArgs e)
+        {
+            txtName.Focus();
+        }
+    }
+}
