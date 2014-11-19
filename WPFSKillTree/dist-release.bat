10c3fba76611627230f48aac2ad027bbcd6fe5cf
l0g0sys <logosys@oneclick.sk>
2014-11-19 08:33:48 +0100

    Added missing configurations to project files of UpdateDB and UnitTests. Added release automation script.

---
+++ WPFSKillTree/dist-release.bat
@@ -0,0 +1,23 @@
+@ECHO OFF
+@REM Release batch script
+
+@REM Check whether Git is found in PATH
+WHERE /Q git
+IF ERRORLEVEL% == 1 (
+	ECHO ERROR: Command git not found.
+	ECHO Please add Git for Windows binaries to PATH environment variable.
+	GOTO :END
+)
+
+@REM Check whether vsvars32 is found in PATH
+WHERE /Q vsvars32
+IF ERRORLEVEL% == 1 (
+	ECHO ERROR: Command vsdevcmd not found.
+	ECHO Please add Visual Studio's Common7\Tools directory to PATH environment variable.
+	GOTO :END
+)
+
+@REM Run in separate process
+cmd /C "vsvars32.bat && msbuild release.xml /target:Release"
+
+:END
