b044b78f99a16767b6203a61f5c0726012269230
Max Langhof <max.langhof@gmx.de>
2015-01-02 13:12:11 +0100

    Node highlighting rework
    Added the possibility to highlight nodes via rightclick.
    Rendering of node highlights has been reworked so that the (now) three
    possible ways to highlight nodes can all be used simultaneously.

---
+++ WPFSKillTree/SkillTreeFiles/NodeHighlighter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace POESKillTree.SkillTreeFiles
+{
+    public class NodeHighlighter
+    {
+        [Flags]
+        public enum HighlightState
+        {
+            FromSearch = 1, FromAttrib = 2, FromNode = 4,
+            All = FromSearch | FromAttrib | FromNode
+        }
+
+        public Dictionary<SkillNode, HighlightState> nodeHighlights = new Dictionary<SkillNode, HighlightState>();
+
+        public void ToggleHighlightNode(SkillNode node, HighlightState toggleFlags)
+        {
+            if (toggleFlags == 0) return;
+            if (nodeHighlights.ContainsKey(node))
+            {
+                nodeHighlights[node] ^= toggleFlags;
+                if (nodeHighlights[node] == 0) nodeHighlights.Remove(node);
+            }
+            else nodeHighlights.Add(node, toggleFlags);
+        }
+
+        public void HighlightNode(SkillNode node, HighlightState newFlags)
+        {
+            if (newFlags == 0) return;
+            if (nodeHighlights.ContainsKey(node))
+                nodeHighlights[node] |= newFlags;
+            else nodeHighlights.Add(node, newFlags);
+        }
+
+        public void UnhighlightNode(SkillNode node, HighlightState removeFlags)
+        {
+            if (nodeHighlights.ContainsKey(node))
+            {
+                // Each flag only remains set if it's not one of the flags to be removed.
+                HighlightState newState = nodeHighlights[node] & ~removeFlags;
+                if (newState == 0) nodeHighlights.Remove(node);
+                else nodeHighlights[node] = newState;
+            }
+        }
+
+        public void HighlightNodes(List<SkillNode> nodes, HighlightState newFlags)
+        {
+            foreach (SkillNode node in nodes)
+                HighlightNode(node, newFlags);
+        }
+
+
+        public void UnhighlightAllNodes(HighlightState removeFlags)
+        {
+            // Kludge cast
+            var keys = nodeHighlights.Keys.ToArray();
+            foreach (SkillNode node in keys)
+                UnhighlightNode(node, removeFlags);
+            //removeUnhighlightedNodes();
+        }
+
+        public void ReplaceHighlights(List<SkillNode> newNodes, HighlightState replaceFlags)
+        {
+            UnhighlightAllNodes(replaceFlags);
+            //removeUnhighlightedNodes();
+            HighlightNodes(newNodes, replaceFlags);
+        }
+    }
+}
