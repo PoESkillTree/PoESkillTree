4420212a8ade2af3b590802c8553ffb68a1a9940
Ttxman <seps.ladislav@seznam.cz>
2015-01-05 23:36:43 +0100

    Basic Tree comparison Selected build (in the saved builds expander) is highlighted (paths and passives) by the green color Attributes in "PassiveAttributes" expander now shows difference to the selected build   - attribute have bigger value than selected build: green colored "+number" at the end of attribute line   - attribute have lesser value than selected build: red colored "-number" at the end of attribute line   - attribute have same value as selected build: nothing :)

---
+++ WPFSKillTree/ViewModels/Converters.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace POESKillTree.ViewModels
+{
+    class SignConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is float)
+            {
+                float val = (float)value;
+                if (targetType == typeof(Brush))
+                {
+                    if (val == 0)
+                        return Brushes.White;
+
+                    if (val > 0)
+                        return Brushes.Green;
+
+                    if (val < 0)
+                        return Brushes.Red;
+                }
+                if (targetType == typeof(Color))
+                {
+                    if (val == 0)
+                        return Colors.White;
+
+                    if (val > 0)
+                        return Colors.Green;
+
+                    if (val < 0)
+                        return Colors.Red;
+                }
+                else if (targetType == typeof(Visibility))
+                {
+                    if (val == 0)
+                        return Visibility.Collapsed;
+
+                    return Visibility.Visible;
+                }
+                else if (targetType == typeof(int))
+                {
+                    if (val == 0)
+                        return 0;
+
+                    if (val > 0)
+                        return 1;
+
+                    if (val < 0)
+                        return -1;
+                }
+
+                throw new NotImplementedException("cannot convert to target type '" + targetType.Name + "'");
+            }
+
+            throw new NotImplementedException("can convert only from floats");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class ArrayToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return Visibility.Collapsed;
+
+            if (value is Array)
+            {
+                bool allzeros = false;
+                if(value is float[])
+                    allzeros = ((float[])value).All(v=>v==0f);
+                else if (value is int[])
+                    allzeros = ((int[])value).All(v => v == 0f);
+                else if (value is double[])
+                    allzeros = ((double[])value).All(v => v == 0.0);
+
+                if (allzeros)
+                    return Visibility.Collapsed;
+
+                if (((Array)value).Length != 0)
+                    return Visibility.Visible;
+            }
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
