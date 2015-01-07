a2f29189810b7ef49a6124d2a266f289c295f8ec
Ttxman <seps.ladislav@seznam.cz>
2015-01-07 23:04:42 +0100

    fix: wordwrap for attributes

--- WPFSKillTree/ViewModels/Converters.cs
+++ WPFSKillTree/ViewModels/Converters.cs
@@ -3,96 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Media;
+using System.Windows.Media.TextFormatting;
 
 namespace POESKillTree.ViewModels
 {
-    class SignConverter : IValueConverter
+    class AttributeToTextblockConverter:IValueConverter
     {
+
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            if (value is float)
+            var attr = value as Attribute;
+            if (attr == null)
+                throw new NotImplementedException();
+
+            var tb = new TextBlock(){TextWrapping= TextWrapping.Wrap};
+            var txt = new Run(attr.Text);
+            if (attr.Missing)
+                txt.Foreground = Brushes.Red;
+
+            tb.Inlines.Add(txt);
+
+            foreach (var i in attr.Deltas)
             {
-                float val = (float)value;
-                if (targetType == typeof(Brush))
+                if (i != 0)
                 {
-                    if (val == 0)
-                        return Brushes.White;
-
-                    if (val > 0)
-                        return Brushes.Green;
-
-                    if (val < 0)
-                        return Brushes.Red;
+                    tb.Inlines.Add(" ");
+                    txt = new Run(i.ToString("+#;-#;0"));
+                    txt.Foreground = (i < 0) ? Brushes.Red : Brushes.Green;
+                    tb.Inlines.Add(txt);
                 }
-                if (targetType == typeof(Color))
-                {
-                    if (val == 0)
-                        return Colors.White;
-
-                    if (val > 0)
-                        return Colors.Green;
-
-                    if (val < 0)
-                        return Colors.Red;
-                }
-                else if (targetType == typeof(Visibility))
-                {
-                    if (val == 0)
-                        return Visibility.Collapsed;
-
-                    return Visibility.Visible;
-                }
-                else if (targetType == typeof(int))
-                {
-                    if (val == 0)
-                        return 0;
-
-                    if (val > 0)
-                        return 1;
-
-                    if (val < 0)
-                        return -1;
-                }
-
-                throw new NotImplementedException("cannot convert to target type '" + targetType.Name + "'");
             }
 
-            throw new NotImplementedException("can convert only from floats");
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    class ArrayToVisibilityConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            if (value == null)
-                return Visibility.Collapsed;
-
-            if (value is Array)
-            {
-                bool allzeros = false;
-                if(value is float[])
-                    allzeros = ((float[])value).All(v=>v==0f);
-                else if (value is int[])
-                    allzeros = ((int[])value).All(v => v == 0f);
-                else if (value is double[])
-                    allzeros = ((double[])value).All(v => v == 0.0);
-
-                if (allzeros)
-                    return Visibility.Collapsed;
-
-                if (((Array)value).Length != 0)
-                    return Visibility.Visible;
-            }
-
-            return Visibility.Collapsed;
+            return tb;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
