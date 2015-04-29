2a652204aeeb6fcd0bac9cee5eb1c52893e4ad84
Ttxman <seps.ladislav@seznam.cz>
2015-04-29 03:39:56 +0200

    Added controls for inventory.

---
+++ WPFSKillTree/Controls/Inventory.xaml.cs
@@ -0,0 +1,37 @@
+﻿using POESKillTree.ViewModels.ItemAttribute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace POESKillTree.Controls
+{
+    /// <summary>
+    /// Interaction logic for Inventory.xaml
+    /// </summary>
+    public partial class Inventory : UserControl
+    {
+        public ItemAttributes ItemAttributes
+        {
+            get { return (ItemAttributes)GetValue(ItemProperty); }
+            set { SetValue(ItemProperty, value); }
+        }
+
+        public static readonly DependencyProperty ItemProperty = DependencyProperty.Register("ItemAttributes", typeof(ItemAttributes), typeof(Inventory), new PropertyMetadata(null));
+
+
+        public Inventory()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WPFSKillTree/Controls/ItemVisualizer.xaml.cs
@@ -0,0 +1,43 @@
+﻿using POESKillTree.ViewModels.ItemAttribute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace POESKillTree.Controls
+{
+    public partial class ItemVisualizer : UserControl
+    {
+        public Item Item
+        {
+            get { return (Item)GetValue(ItemProperty); }
+            set { SetValue(ItemProperty, value); }
+        }
+
+        public static readonly DependencyProperty ItemProperty = DependencyProperty.Register("Item", typeof(Item), typeof(ItemVisualizer), new PropertyMetadata(null));
+
+
+        public ItemVisualizer()
+        {
+            InitializeComponent();
+        }
+
+        private void control_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            var pos = e.GetPosition(this);
+
+            tooltip.VerticalOffset = pos.Y+2;
+            tooltip.HorizontalOffset = pos.X;
+        }
+    }
+}
