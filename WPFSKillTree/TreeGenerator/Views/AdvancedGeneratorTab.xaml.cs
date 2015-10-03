53e187914ae14b68cad36f6f402ea057ecd7972c
brather1ng <github@soeren-thomas.de>
2015-10-03 16:46:18 +0200

    Made DataGrid cells always editable
    (without the need of extra clicks)

--- WPFSKillTree/TreeGenerator/Views/AdvancedGeneratorTab.xaml.cs
+++ WPFSKillTree/TreeGenerator/Views/AdvancedGeneratorTab.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System.Windows.Controls;
-using System.Windows.Input;
-using POESKillTree.Utils;
-
-namespace POESKillTree.TreeGenerator.Views
+﻿namespace POESKillTree.TreeGenerator.Views
 {
     /// <summary>
     /// Interaction logic for AdvancedGeneratorTab.xaml
@@ -12,38 +8,6 @@ namespace POESKillTree.TreeGenerator.Views
         public AdvancedGeneratorTab()
         {
             InitializeComponent();
-        }
-        
-        /// <summary>
-        /// Forces the DataGridCell into Edit mode with a single click instead of the normal double click.
-        /// </summary>
-        private void DataGridCell_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            var cell = sender as DataGridCell;
-            if (cell != null && !cell.IsEditing && !cell.IsReadOnly)
-            {
-                if (!cell.IsFocused)
-                {
-                    cell.Focus();
-                }
-                var dataGrid = cell.FindAnchestor<DataGrid>();
-                if (dataGrid != null)
-                {
-                    if (dataGrid.SelectionUnit != DataGridSelectionUnit.FullRow)
-                    {
-                        if (!cell.IsSelected)
-                            cell.IsSelected = true;
-                    }
-                    else
-                    {
-                        var row = cell.FindAnchestor<DataGridRow>();
-                        if (row != null && !row.IsSelected)
-                        {
-                            row.IsSelected = true;
-                        }
-                    }
-                }
-            }
         }
     }
 }
