685e0e405cd8f341517c47843925ca4b6d5c1fc0
EmmittJ <1337ERJ1337@gmail.com>
2016-03-04 19:24:38 -0800

    Forgot a file for the last commit

--- WPFSKillTree/Model/PersistentData.cs
+++ WPFSKillTree/Model/PersistentData.cs
@@ -38,7 +38,7 @@ namespace POESKillTree.Model
             CurrentBuild = new PoEBuild
             {
                 Name = "New Build",
-                Url = SkillTree.TreeAddress + SkillTree.GetCharacterURL(3),
+                Url = SkillTree.TreeAddress + SkillTree.GetCharacterURL(3, 0),
                 Level = "1"
             };
             Builds = new List<PoEBuild>();
