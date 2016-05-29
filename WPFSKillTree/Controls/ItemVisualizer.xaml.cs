e1f0c4206e2f63e79d0367492134c9e5e62db24a
brather1ng <github@soeren-thomas.de>
2016-05-29 02:08:17 +0200

    Item tooltip related fixes:
    - Fix slow stash scrolling with a fair amount of items
    - Fix flickering item tooltips when above the cursor (not below)
    - Fix unreadable tooltips on light theme

--- WPFSKillTree/Controls/ItemVisualizer.xaml.cs
+++ WPFSKillTree/Controls/ItemVisualizer.xaml.cs
@@ -1,12 +1,12 @@
 ﻿using System.Windows;
-using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media;
 using POESKillTree.Model.Items;
 
 namespace POESKillTree.Controls
 {
-    public partial class ItemVisualizer : UserControl
+    public partial class ItemVisualizer
     {
         public static readonly Brush FoundItemBrush = new SolidColorBrush(Color.FromArgb(0x80,0x43,0xD9,0xE8));
 
@@ -16,12 +16,20 @@ namespace POESKillTree.Controls
             set { SetValue(ItemProperty, value); }
         }
 
-        public static readonly DependencyProperty ItemProperty = DependencyProperty.Register("Item", typeof(Item), typeof(ItemVisualizer), new PropertyMetadata(null));
-
+        public static readonly DependencyProperty ItemProperty = DependencyProperty.Register("Item", typeof(Item),
+            typeof(ItemVisualizer), new PropertyMetadata(null));
 
         public ItemVisualizer()
         {
             InitializeComponent();
+            // The ItemTooltip is lazily created because it is a major slowdown if all tooltips are recreated for each scroll.
+            tooltip.Opened += (sender, args) =>
+            {
+                if (tooltip.Child != null) return;
+                var itemTooltip = new ItemTooltip {Item = Item};
+                itemTooltip.SetBinding(ItemTooltip.ItemProperty, new Binding("Item") {Source = this});
+                tooltip.Child = itemTooltip;
+            };
         }
 
         private void control_PreviewMouseMove(object sender, MouseEventArgs e)
@@ -29,7 +37,7 @@ namespace POESKillTree.Controls
             var pos = e.GetPosition(this);
 
             tooltip.VerticalOffset = pos.Y + 2;
-            tooltip.HorizontalOffset = pos.X;
+            tooltip.HorizontalOffset = pos.X + 2;
         }
     }
 }
