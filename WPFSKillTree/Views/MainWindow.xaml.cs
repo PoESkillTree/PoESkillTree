6538e36dee22fc774f9a097df23d33e3a6b190b1
Daniel Mauritzson <daniel.mauritzson@gmail.com>
2016-09-06 07:42:44 +0200

    Fixed so "All" is selected in filter from start

--- WPFSKillTree/Views/MainWindow.xaml.cs
+++ WPFSKillTree/Views/MainWindow.xaml.cs
@@ -487,7 +487,7 @@ namespace POESKillTree.Views
             Tree.PropertyChanged += Tree_PropertyChanged;
             PopulateAsendancySelectionList();
 
-            var classesForFilter = new List<ComboBoxItem> {new ComboBoxItem {Content = "All", IsSelected = true}};
+            var classesForFilter = new List<ComboBoxItem> {new ComboBoxItem {Content = "All"}};
             foreach (var nameToContent in CharacterNames.NameToContent)
             {
                 classesForFilter.Add(new ComboBoxItem { Name = nameToContent.Value, Content = nameToContent.Value });
@@ -501,6 +501,7 @@ namespace POESKillTree.Views
                         }));
             }
             cbCharTypeSavedBuildFilter.ItemsSource = classesForFilter;
+            cbCharTypeSavedBuildFilter.SelectedIndex = 0;
 
             await CheckAppVersionAndDoNecessaryChanges();
 
