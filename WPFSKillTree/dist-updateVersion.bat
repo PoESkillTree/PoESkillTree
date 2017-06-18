d118eca43248a936411208975f4ee27149066b96
brather1ng <github@soeren-thomas.de>
2017-06-18 23:19:08 +0200

    Change release.xml scripts to work with Visual Studio 2017

--- WPFSKillTree/dist-updateVersion.bat
+++ WPFSKillTree/dist-updateVersion.bat
@@ -10,8 +10,8 @@ IF ERRORLEVEL% == 1 (
 	GOTO :END
 )
 
-@REM Check whether vsvars32 is found in PATH
-WHERE /Q vsvars32
+@REM Check whether vsdevcmd is found in PATH
+WHERE /Q vsdevcmd
 IF ERRORLEVEL% == 1 (
 	ECHO ERROR: Command vsdevcmd not found.
 	ECHO Please add Visual Studio's Common7\Tools directory to PATH environment variable.
@@ -19,7 +19,7 @@ IF ERRORLEVEL% == 1 (
 )
 
 @REM Run in separate process
-cmd /C "vsvars32.bat && msbuild release.xml /target:UpdateAssemblyInfo"
-cmd /C "vsvars32.bat && msbuild release.xml /target:Clean"
+cmd /C "vsdevcmd.bat && cd ""%~dp0"" && msbuild release.xml /target:UpdateAssemblyInfo"
+cmd /C "vsdevcmd.bat && cd ""%~dp0"" && msbuild release.xml /target:Clean"
 
 :END
