caf23ccf6f1d81a0862b1cbc0857393e34cbf259
brather1ng <github@soeren-thomas.de>
2018-01-14 18:38:24 +0100

    Rename Computation project to Computation.Core
    This project is where this branch's code will go.
    
    (last commit said it removed the project. I thought I removed it, but somehow I didn't)

--- PoESkillTree.Computation.Core/Properties/AssemblyInfo.cs
+++ PoESkillTree.Computation.Core/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PoESkillTree.Computation")]
+[assembly: AssemblyTitle("PoESkillTree.Computation.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PoESkillTree.Computation")]
+[assembly: AssemblyProduct("PoESkillTree.Computation.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
