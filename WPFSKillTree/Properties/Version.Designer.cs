80dc50f0f3cd58e61a53103aa5af913a35d8212d
EmmittJ <1337ERJ1337@gmail.com>
2018-05-30 12:36:01 -0700

    Release 2.7.6

--- WPFSKillTree/Properties/Version.Designer.cs
+++ WPFSKillTree/Properties/Version.Designer.cs
@@ -133,7 +133,7 @@ namespace POESKillTree.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 2.7.5.
+        ///   Looks up a localized string similar to 2.7.6.
         /// </summary>
         public static string ProductVersion {
             get {
