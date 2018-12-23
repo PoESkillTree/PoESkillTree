be8d49856a40d612a554b9868f82520d75ee4e3c
brather1ng <github@soeren-thomas.de>
2018-12-23 23:25:39 +0100

    Add ModifierLocalityTester.AffectsProperties()

--- PoESkillTree.GameModel.Tests/Modifiers/ModifierLocalityTesterTest.cs
+++ PoESkillTree.GameModel.Tests/Modifiers/ModifierLocalityTesterTest.cs
@@ -19,6 +19,7 @@ namespace PoESkillTree.GameModel.Tests.Modifiers
         [TestCase("1% chance to maim on hit", ExpectedResult = true)]
         [TestCase("maim on hit", ExpectedResult = true)]
         [TestCase("Adds # to # Physical Damage to Attacks with this Weapon per 3 Player Levels", ExpectedResult = true)]
+        [TestCase("Attacks with this Weapon Maim on hit", ExpectedResult = true)]
         public bool IsLocalReturnsCorrectResultForWeapon(string modifier)
             => ModifierLocalityTester.IsLocal(modifier, Tags.Weapon);
 
@@ -41,5 +42,35 @@ namespace PoESkillTree.GameModel.Tests.Modifiers
         [TestCase("Removes Burning on use", ExpectedResult = false)]
         public bool IsLocalReturnsCorrectResultForFlask(string modifier)
             => ModifierLocalityTester.IsLocal(modifier, Tags.Flask);
+
+        [TestCase("+20% to Quality", ExpectedResult = true)]
+        [TestCase("+10 to maximum Life", ExpectedResult = false)]
+        public bool AffectsPropertiesReturnsCorrectResultForGeneric(string modifier)
+            => ModifierLocalityTester.AffectsProperties(modifier, Tags.Default);
+
+        [TestCase("Adds 2 to 6 Physical Damage", ExpectedResult = true)]
+        [TestCase("Adds 2 to 6 Physical Damage to Attacks", ExpectedResult = false)]
+        [TestCase("1% chance to maim on hit", ExpectedResult = false)]
+        [TestCase("Adds # to # Physical Damage to Attacks with this Weapon per 3 Player Levels", ExpectedResult =
+            true)]
+        [TestCase("Attacks with this Weapon Maim on hit", ExpectedResult = false)]
+        public bool AffectsPropertiesReturnsCorrectResultForWeapon(string modifier)
+            => ModifierLocalityTester.AffectsProperties(modifier, Tags.Weapon);
+
+        [TestCase("+100 to Energy Shield", ExpectedResult = true)]
+        [TestCase("1% increased Physical Damage", ExpectedResult = false)]
+        public bool AffectsPropertiesReturnsCorrectResultForArmour(string modifier)
+            => ModifierLocalityTester.AffectsProperties(modifier, Tags.Armour);
+
+        [TestCase("+5% Chance to Block", ExpectedResult = true)]
+        [TestCase("+25% Chance to Block Projectile Attack Damage", ExpectedResult = false)]
+        public bool AffectsPropertiesReturnsCorrectResultForShield(string modifier)
+            => ModifierLocalityTester.AffectsProperties(modifier, Tags.Shield);
+
+        [TestCase("10% increased effect", ExpectedResult = true)]
+        [TestCase("1% increased charge recovery", ExpectedResult = false)]
+        [TestCase("Removes Burning on use", ExpectedResult = false)]
+        public bool AffectsPropertiesReturnsCorrectResultForFlask(string modifier)
+            => ModifierLocalityTester.AffectsProperties(modifier, Tags.Flask);
     }
 }
--- PoESkillTree.GameModel/Modifiers/ModifierLocalityTester.cs
+++ PoESkillTree.GameModel/Modifiers/ModifierLocalityTester.cs
@@ -14,18 +14,24 @@ namespace PoESkillTree.GameModel.Modifiers
         private const string Chance = @"\+?#% chance to ";
         private static readonly string OptionalChance = $"({Chance})?";
 
-        private static readonly IReadOnlyList<Regex> LocalUnconditionally = new RegexCollection
+        private static readonly IReadOnlyList<Regex> UnconditionalProperty = new RegexCollection
         {
             "level requirement (increased|reduced) by #",
             BaseAdd + "quality",
             @"(has no|no|#% increased|#% reduced|\+#|-#) (attribute|intelligence|dexterity|strength) requirements?",
         }.ToList();
 
-        private static readonly IReadOnlyList<Regex> WeaponLocal = new RegexCollection
+        private static readonly IReadOnlyList<Regex> WeaponProperty = new RegexCollection
         {
             "adds # to # (physical|fire|cold|lightning|chaos) damage( in (main|off) hand)?",
             "no physical damage",
             Increase + "(physical damage|attack speed|critical strike chance|weapon range)",
+            "adds # to # physical damage to attacks with this weapon.*",
+            "attacks with this weapon deal # to # added (physical|fire) damage.*",
+        }.ToList();
+
+        private static readonly IReadOnlyList<Regex> WeaponLocal = new RegexCollection
+        {
             OptionalChance + "impale enemies on hit with attacks",
             OptionalChance + "(blind enemies|poison|causes? bleeding|maim) on hit",
             OptionalChance + "cause (poison|bleeding) on critical strike",
@@ -40,24 +46,28 @@ namespace PoESkillTree.GameModel.Modifiers
             Increase + "enemy stun threshold with this weapon",
         }.ToList();
 
-        private static readonly IReadOnlyList<Regex> ArmourLocal = new RegexCollection
+        private static readonly IReadOnlyList<Regex> ArmourProperty = new RegexCollection
         {
             $"({Increase}|{BaseAdd})" +
             "(armour|evasion|energy shield|armour and evasion|armour and energy shield|evasion and energy shield|armour, evasion and energy Shield)",
             "item has no level requirement and energy shield",
         }.ToList();
 
-        private static readonly IReadOnlyList<Regex> ShieldLocal = new RegexCollection
+        private static readonly IReadOnlyList<Regex> ShieldProperty = new RegexCollection
         {
             Chance + "block",
             "cannot block",
             "no chance to block",
         }.ToList();
 
-        private static readonly IReadOnlyList<Regex> FlaskLocal = new RegexCollection
+        private static readonly IReadOnlyList<Regex> FlaskProperty = new RegexCollection
         {
             Increase + "(recovery rate|(life|mana|amount) recovered|effect|duration|charges used)",
             BaseAdd + "maximum charges",
+        }.ToList();
+
+        private static readonly IReadOnlyList<Regex> FlaskLocal = new RegexCollection
+        {
             Increase + "(charge recovery|recovery when on low life)",
             OptionalChance + "gain a flask charge when you deal a critical strike",
             "gains no charges during effect of any (soul ripper|overflowing chalice) flask",
@@ -69,16 +79,29 @@ namespace PoESkillTree.GameModel.Modifiers
 
         private static readonly Regex NumberRegex = new Regex(@"\d+(\.\d+)?");
 
-        public static bool IsLocal(string modifier, Tags itemTags)
+        public static bool AffectsProperties(string modifier, Tags itemTags)
         {
             var canonicalModifier = NumberRegex.Replace(modifier, "#").ToLowerInvariant();
-            if (LocalUnconditionally.Any(r => r.IsMatch(canonicalModifier)))
+            if (UnconditionalProperty.Any(r => r.IsMatch(canonicalModifier)))
                 return true;
+            if (itemTags.HasFlag(Tags.Weapon) && WeaponProperty.Any(r => r.IsMatch(canonicalModifier)))
+                return true;
+            if (itemTags.HasFlag(Tags.Armour) && ArmourProperty.Any(r => r.IsMatch(canonicalModifier)))
+                return true;
+            if (itemTags.HasFlag(Tags.Shield) && ShieldProperty.Any(r => r.IsMatch(canonicalModifier)))
+                return true;
+            if (itemTags.HasFlag(Tags.Flask) && FlaskProperty.Any(r => r.IsMatch(canonicalModifier)))
+                return true;
+            return false;
+        }
+
+        public static bool IsLocal(string modifier, Tags itemTags)
+        {
+            if (AffectsProperties(modifier, itemTags))
+                return true;
+
+            var canonicalModifier = NumberRegex.Replace(modifier, "#").ToLowerInvariant();
             if (itemTags.HasFlag(Tags.Weapon) && WeaponLocal.Any(r => r.IsMatch(canonicalModifier)))
-                return true;
-            if (itemTags.HasFlag(Tags.Armour) && ArmourLocal.Any(r => r.IsMatch(canonicalModifier)))
-                return true;
-            if (itemTags.HasFlag(Tags.Shield) && ShieldLocal.Any(r => r.IsMatch(canonicalModifier)))
                 return true;
             if (itemTags.HasFlag(Tags.Flask) && FlaskLocal.Any(r => r.IsMatch(canonicalModifier)))
                 return true;
