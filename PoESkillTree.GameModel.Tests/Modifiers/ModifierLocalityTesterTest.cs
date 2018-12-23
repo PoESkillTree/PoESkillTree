ee111a0b42609822c9480ad1daa735a88f1456bb
brather1ng <github@soeren-thomas.de>
2018-12-23 23:09:12 +0100

    Add ModifierLocalityTester.IsLocal()

---
+++ PoESkillTree.GameModel.Tests/Modifiers/ModifierLocalityTesterTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using PoESkillTree.GameModel.Items;
+using PoESkillTree.GameModel.Modifiers;
+
+namespace PoESkillTree.GameModel.Tests.Modifiers
+{
+    [TestFixture]
+    public class ModifierLocalityTesterTest
+    {
+        [TestCase("+20% to Quality", ExpectedResult = true)]
+        [TestCase("+10 to maximum Life", ExpectedResult = false)]
+        public bool IsLocalReturnsCorrectResultForGeneric(string modifier)
+            => ModifierLocalityTester.IsLocal(modifier, Tags.Default);
+
+        [TestCase("Adds 2 to 6 Physical Damage", ExpectedResult = true)]
+        [TestCase("adds 2 to 6 physical damage", ExpectedResult = true)]
+        [TestCase("Adds 2 to 6 Physical Damage to Attacks", ExpectedResult = false)]
+        [TestCase("1% increased Physical Damage", ExpectedResult = true)]
+        [TestCase("1% chance to maim on hit", ExpectedResult = true)]
+        [TestCase("maim on hit", ExpectedResult = true)]
+        [TestCase("Adds # to # Physical Damage to Attacks with this Weapon per 3 Player Levels", ExpectedResult = true)]
+        public bool IsLocalReturnsCorrectResultForWeapon(string modifier)
+            => ModifierLocalityTester.IsLocal(modifier, Tags.Weapon);
+
+        [TestCase("+100 to Energy Shield", ExpectedResult = true)]
+        [TestCase("-100 to Energy Shield", ExpectedResult = true)]
+        [TestCase("10% increased Energy Shield", ExpectedResult = true)]
+        [TestCase("10% reduced Energy Shield", ExpectedResult = true)]
+        [TestCase("10% increased maximum Energy Shield", ExpectedResult = false)]
+        [TestCase("1% increased Physical Damage", ExpectedResult = false)]
+        public bool IsLocalReturnsCorrectResultForArmour(string modifier)
+            => ModifierLocalityTester.IsLocal(modifier, Tags.Armour);
+
+        [TestCase("+5% Chance to Block", ExpectedResult = true)]
+        [TestCase("+25% Chance to Block Projectile Attack Damage", ExpectedResult = false)]
+        public bool IsLocalReturnsCorrectResultForShield(string modifier)
+            => ModifierLocalityTester.IsLocal(modifier, Tags.Shield);
+
+        [TestCase("10% increased effect", ExpectedResult = true)]
+        [TestCase("1% increased charge recovery", ExpectedResult = true)]
+        [TestCase("Removes Burning on use", ExpectedResult = false)]
+        public bool IsLocalReturnsCorrectResultForFlask(string modifier)
+            => ModifierLocalityTester.IsLocal(modifier, Tags.Flask);
+    }
+}
---
+++ PoESkillTree.GameModel/Modifiers/ModifierLocalityTester.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using JetBrains.Annotations;
+using PoESkillTree.GameModel.Items;
+
+namespace PoESkillTree.GameModel.Modifiers
+{
+    public static class ModifierLocalityTester
+    {
+        private const string Increase = "#% (increased|reduced) ";
+        private const string BaseAdd = @"(\+|-)?#%? to ";
+        private const string Chance = @"\+?#% chance to ";
+        private static readonly string OptionalChance = $"({Chance})?";
+
+        private static readonly IReadOnlyList<Regex> LocalUnconditionally = new RegexCollection
+        {
+            "level requirement (increased|reduced) by #",
+            BaseAdd + "quality",
+            @"(has no|no|#% increased|#% reduced|\+#|-#) (attribute|intelligence|dexterity|strength) requirements?",
+        }.ToList();
+
+        private static readonly IReadOnlyList<Regex> WeaponLocal = new RegexCollection
+        {
+            "adds # to # (physical|fire|cold|lightning|chaos) damage( in (main|off) hand)?",
+            "no physical damage",
+            Increase + "(physical damage|attack speed|critical strike chance|weapon range)",
+            OptionalChance + "impale enemies on hit with attacks",
+            OptionalChance + "(blind enemies|poison|causes? bleeding|maim) on hit",
+            OptionalChance + "cause (poison|bleeding) on critical strike",
+            "always poison on hit",
+            "#% of physical attack damage leched as (life|mana|life and mana)",
+            BaseAdd + "accuracy rating",
+            "hits can't be evaded",
+            "# (life|mana) gained for each enemy hit by attacks",
+            "# life and mana gained for each enemy hit",
+            ".*(attacks|hits|on hit|inflicted|critical strikes) with this weapon.*",
+            ".*hit by this weapon.*",
+            Increase + "enemy stun threshold with this weapon",
+        }.ToList();
+
+        private static readonly IReadOnlyList<Regex> ArmourLocal = new RegexCollection
+        {
+            $"({Increase}|{BaseAdd})" +
+            "(armour|evasion|energy shield|armour and evasion|armour and energy shield|evasion and energy shield|armour, evasion and energy Shield)",
+            "item has no level requirement and energy shield",
+        }.ToList();
+
+        private static readonly IReadOnlyList<Regex> ShieldLocal = new RegexCollection
+        {
+            Chance + "block",
+            "cannot block",
+            "no chance to block",
+        }.ToList();
+
+        private static readonly IReadOnlyList<Regex> FlaskLocal = new RegexCollection
+        {
+            Increase + "(recovery rate|(life|mana|amount) recovered|effect|duration|charges used)",
+            BaseAdd + "maximum charges",
+            Increase + "(charge recovery|recovery when on low life)",
+            OptionalChance + "gain a flask charge when you deal a critical strike",
+            "gains no charges during effect of any (soul ripper|overflowing chalice) flask",
+            "recharges # charges? when you ((deal|take) a critical strike|consume an ignited corpse)",
+            "instant recovery(when on low life)?",
+            "#% of recovery applied instantly",
+            "grants #% of life recovery to minions",
+        }.ToList();
+
+        private static readonly Regex NumberRegex = new Regex(@"\d+(\.\d+)?");
+
+        public static bool IsLocal(string modifier, Tags itemTags)
+        {
+            var canonicalModifier = NumberRegex.Replace(modifier, "#").ToLowerInvariant();
+            if (LocalUnconditionally.Any(r => r.IsMatch(canonicalModifier)))
+                return true;
+            if (itemTags.HasFlag(Tags.Weapon) && WeaponLocal.Any(r => r.IsMatch(canonicalModifier)))
+                return true;
+            if (itemTags.HasFlag(Tags.Armour) && ArmourLocal.Any(r => r.IsMatch(canonicalModifier)))
+                return true;
+            if (itemTags.HasFlag(Tags.Shield) && ShieldLocal.Any(r => r.IsMatch(canonicalModifier)))
+                return true;
+            if (itemTags.HasFlag(Tags.Flask) && FlaskLocal.Any(r => r.IsMatch(canonicalModifier)))
+                return true;
+            return false;
+        }
+
+        private class RegexCollection : IEnumerable<Regex>
+        {
+            private readonly List<Regex> _regexes = new List<Regex>();
+
+            public void Add([RegexPattern] string pattern)
+                => _regexes.Add(new Regex($"^{pattern}$", RegexOptions.Multiline | RegexOptions.IgnoreCase));
+
+            public IEnumerator<Regex> GetEnumerator() => _regexes.GetEnumerator();
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+    }
+}
