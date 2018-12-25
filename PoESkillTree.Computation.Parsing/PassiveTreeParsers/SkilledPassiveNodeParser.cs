7858d60958e5f6104d5268c4cf3dab6eebf45049
brather1ng <github@soeren-thomas.de>
2018-12-26 00:39:03 +0100

    Add SkilledPassiveNodeParser for enabling/skilling previously parsed nodes

---
+++ PoESkillTree.Computation.Parsing/PassiveTreeParsers/SkilledPassiveNodeParser.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using PoESkillTree.Computation.Common;
+using PoESkillTree.Computation.Common.Builders;
+using PoESkillTree.GameModel.PassiveTree;
+
+namespace PoESkillTree.Computation.Parsing.PassiveTreeParsers
+{
+    public class SkilledPassiveNodeParser : IParser<ushort>
+    {
+        private readonly PassiveTreeDefinition _passiveTreeDefinition;
+        private readonly IBuilderFactories _builderFactories;
+
+        public SkilledPassiveNodeParser(
+            PassiveTreeDefinition passiveTreeDefinition, IBuilderFactories builderFactories)
+            => (_passiveTreeDefinition, _builderFactories) = (passiveTreeDefinition, builderFactories);
+
+        public ParseResult Parse(ushort nodeId)
+        {
+            var nodeDefinition = _passiveTreeDefinition.GetNodeById(nodeId);
+            var localSource = new ModifierSource.Local.Tree(nodeDefinition.Name);
+            var modifiers = new ModifierCollection(_builderFactories, localSource);
+            modifiers.AddGlobal(_builderFactories.StatBuilders.PassiveNodeSkilled(nodeId), Form.TotalOverride, 1);
+            return ParseResult.Success(modifiers.ToList());
+        }
+    }
+}
