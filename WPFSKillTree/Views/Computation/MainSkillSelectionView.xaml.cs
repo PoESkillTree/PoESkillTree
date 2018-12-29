2ffb53f2f247b0bf368dfe58058ab770f480e84d
brather1ng <github@soeren-thomas.de>
2018-12-29 22:47:11 +0100

    Add main skill selection options to ComputationView

---
+++ WPFSKillTree/Views/Computation/MainSkillSelectionView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace POESKillTree.Views.Computation
+{
+    /// <summary>
+    /// Interaction logic for MainSkillSelectionView.xaml
+    /// </summary>
+    public partial class MainSkillSelectionView
+    {
+        public MainSkillSelectionView()
+        {
+            InitializeComponent();
+        }
+    }
+}
