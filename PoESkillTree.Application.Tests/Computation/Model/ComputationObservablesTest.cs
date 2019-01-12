9478b29bee673623f454063897e440a9add9e895
brather1ng <7596346+brather1ng@users.noreply.github.com>
2019-01-13 00:36:20 +0100

    Add NUnit test project for WPFSKillTree

---
+++ PoESkillTree.Application.Tests/Computation/Model/ComputationObservablesTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace PoESkillTree.Application.Tests.Computation.Model
+{
+    [TestFixture]
+    public class ComputationObservablesTest
+    {
+        [Test]
+        public void METHOD()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
