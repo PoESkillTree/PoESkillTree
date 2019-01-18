38b5309d8fdaf51863efe644453f03498ff426f9
brather1ng <7596346+brather1ng@users.noreply.github.com>
2019-01-18 21:41:24 +0100

    Refactor BuildUrlNormalizerTest to NUnit

---
+++ WPFSKillTree.Tests/Utils/UrlProcessing/BuildUrlNormalizerExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using POESKillTree.Utils.UrlProcessing;
+
+namespace PoESkillTree.Tests.Utils.UrlProcessing
+{
+    internal static class BuildUrlNormalizerExtensions
+    {
+        public static Task<string> NormalizeAsync(this BuildUrlNormalizer @this, string buildUrl)
+            => @this.NormalizeAsync(buildUrl, (_, t) => t);
+    }
+}
