20f84ac25522388ca5d1930158dc3c451ef07fbb
brather1ng <7596346+brather1ng@users.noreply.github.com>
2019-02-25 22:40:26 +0100

    Add generic INotifyCollectionChanged
    - Added and removed items are typed
    - Does not have an action enum, just added and removed items. There's no "Refresh" action, making it better suited for difference based subscribers.

---
+++ PoESkillTree.Utils/CollectionChangedEventArgs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PoESkillTree.Utils
+{
+    public static class CollectionChangedEventArgs
+    {
+        public static CollectionChangedEventArgs<T> Added<T>(T addedItem)
+            => Added((IReadOnlyCollection<T>) new[] { addedItem });
+
+        public static CollectionChangedEventArgs<T> Added<T>(IReadOnlyCollection<T> addedItems)
+            => Replaced(addedItems, new T[0]);
+
+        public static CollectionChangedEventArgs<T> Removed<T>(T removedItem)
+            => Removed((IReadOnlyCollection<T>) new[] { removedItem });
+
+        public static CollectionChangedEventArgs<T> Removed<T>(IReadOnlyCollection<T> removedItems)
+            => Replaced(new T[0], removedItems);
+
+        public static CollectionChangedEventArgs<T> Replaced<T>(
+            IReadOnlyCollection<T> addedItems, IReadOnlyCollection<T> removedItems)
+            => new CollectionChangedEventArgs<T>(addedItems, removedItems);
+    }
+
+    public class CollectionChangedEventArgs<T> : EventArgs
+    {
+        public CollectionChangedEventArgs(IReadOnlyCollection<T> addedItems, IReadOnlyCollection<T> removedItems)
+        {
+            AddedItems = addedItems;
+            RemovedItems = removedItems;
+        }
+
+        public IReadOnlyCollection<T> AddedItems { get; }
+        public IReadOnlyCollection<T> RemovedItems { get; }
+    }
+}
---
+++ PoESkillTree.Utils/INotifyCollectionChanged.cs
@@ -0,0 +1,9 @@
+﻿namespace PoESkillTree.Utils
+{
+    public interface INotifyCollectionChanged<T>
+    {
+        event CollectionChangedEventHandler<T> CollectionChanged;
+    }
+
+    public delegate void CollectionChangedEventHandler<T>(object sender, CollectionChangedEventArgs<T> args);
+}
