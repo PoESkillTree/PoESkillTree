11307a411f5df8bfc87cda39c903e4ad473f962b
EmmittJ <1337ERJ1337@gmail.com>
2019-06-03 22:29:48 -0700

    Release 3.0.0-alpha.3 - Preparing for 3.7.0 skill tree data

--- WPFSKillTree/Properties/Version.Designer.cs
+++ WPFSKillTree/Properties/Version.Designer.cs
@@ -19,7 +19,7 @@ namespace PoESkillTree.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Version {
@@ -106,7 +106,7 @@ namespace PoESkillTree.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 3.6.0.
+        ///   Looks up a localized string similar to 3.7.0.
         /// </summary>
         public static string GGGPatchVersion {
             get {
@@ -133,7 +133,7 @@ namespace PoESkillTree.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 3.0.0-alpha.2.
+        ///   Looks up a localized string similar to 3.0.0-alpha.3.
         /// </summary>
         public static string ProductVersion {
             get {
