9fc54136e94f06265295e75e121bff43f1b9e3ca
brather1ng <7596346+brather1ng@users.noreply.github.com>
2019-07-29 22:04:54 +0200

    Add jewels socketed in items

--- WPFSKillTree/ViewModels/Equipment/InventoryViewModel.cs
+++ WPFSKillTree/ViewModels/Equipment/InventoryViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using MoreLinq;
 using PoESkillTree.Engine.GameModel.Items;
+using PoESkillTree.Engine.Utils.Extensions;
 using PoESkillTree.Model.Items;
 using PoESkillTree.Utils;
 
@@ -28,6 +29,7 @@ namespace PoESkillTree.ViewModels.Equipment
         public InventoryItemViewModel Belt { get; }
         public IReadOnlyList<InventoryItemViewModel> Flasks { get; }
         public IReadOnlyList<InventoryItemViewModel> TreeJewels { get; }
+        public IReadOnlyDictionary<ItemSlot, IReadOnlyList<InventoryItemViewModel>> ItemJewels { get; }
 
         public InventoryViewModel(
             IExtendedDialogCoordinator dialogCoordinator,
@@ -36,6 +38,7 @@ namespace PoESkillTree.ViewModels.Equipment
         {
             _dialogCoordinator = dialogCoordinator;
             _itemAttributes = itemAttributes;
+
             Armor = CreateSlotVm(ItemSlot.BodyArmour);
             MainHand = CreateSlotVm(ItemSlot.MainHand);
             OffHand = CreateSlotVm(ItemSlot.OffHand);
@@ -46,21 +49,38 @@ namespace PoESkillTree.ViewModels.Equipment
             Gloves = CreateSlotVm(ItemSlot.Gloves);
             Boots = CreateSlotVm(ItemSlot.Boots);
             Belt = CreateSlotVm(ItemSlot.Belt);
+
             Flasks = ItemSlotExtensions.Flasks.Select(s => CreateSlotVm(s)).ToList();
+
             TreeJewels = jewelPassiveNodes.Select(i => CreateSlotVm(ItemSlot.SkillTree, i)).ToList();
+            ItemJewels = new Dictionary<ItemSlot, IReadOnlyList<InventoryItemViewModel>>
+            {
+                [ItemSlot.BodyArmour] = CreateItemJewelVms(ItemSlot.BodyArmour, 1),
+                [ItemSlot.Helm] = CreateItemJewelVms(ItemSlot.Helm, 2),
+                [ItemSlot.Gloves] = CreateItemJewelVms(ItemSlot.Gloves, 2),
+                [ItemSlot.Boots] = CreateItemJewelVms(ItemSlot.Boots, 2),
+                [ItemSlot.Belt] = CreateItemJewelVms(ItemSlot.Belt, 2),
+            };
         }
+
+        private IReadOnlyList<InventoryItemViewModel> CreateItemJewelVms(ItemSlot slot, int count)
+            => Enumerable.Range(0, count)
+                .Select(i => CreateSlotVm(slot, (ushort) i))
+                .ToList();
 
         private InventoryItemViewModel CreateSlotVm(ItemSlot slot, ushort? socket = null)
         {
-            var imageName = SlotToImageName(slot);
+            var imageName = SlotToImageName(slot, socket.HasValue);
             return new InventoryItemViewModel(_dialogCoordinator, _itemAttributes, slot, socket)
             {
                 EmptyBackgroundImagePath = $"/PoESkillTree;component/Images/EquipmentUI/ItemDefaults/{imageName}.png"
             };
         }
 
-        private static string SlotToImageName(ItemSlot slot)
+        private static string SlotToImageName(ItemSlot slot, bool isSocketed)
         {
+            if (isSocketed)
+                return slot == ItemSlot.SkillTree ? "Jewel" : "AbyssJewel";
             if (slot.IsFlask())
                 return "LifeFlask";
             switch (slot)
@@ -73,8 +93,6 @@ namespace PoESkillTree.ViewModels.Equipment
                     return "Ring";
                 case ItemSlot.Helm:
                     return "Helmet";
-                case ItemSlot.SkillTree:
-                    return "Jewel";
                 default:
                     return slot.ToString();
             }
@@ -94,6 +112,7 @@ namespace PoESkillTree.ViewModels.Equipment
             Belt.Dispose();
             Flasks.ForEach(vm => vm.Dispose());
             TreeJewels.ForEach(vm => vm.Dispose());
+            ItemJewels.Values.Flatten().ForEach(vm => vm.Dispose());
         }
     }
 }
